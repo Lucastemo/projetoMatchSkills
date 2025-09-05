54f6df018c02eb2050e0ea8d3f88b1c8f3ae2c7f
jhonny-marques <jhonnymrgs@gmail.com>
2025-09-05 00:38:12 -0300

    Atualizações na estrutura do projeto e inserção das procedures referente a criação dos usuários.

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT
-//app.use(express.json());
+
+app.use(express.json());
 
 app.listen(PORT, () => {
     console.log('Servidor OK.');
