7a9f118d01ac0dca410338169d8efcca0ad24775
LucasTM <lucasteixeiramoraes15@gmail.com>
2025-11-02 19:01:04 -0300

    Organizar as rotas para o front-end do projeto nos seus respectivos arquivos na pasta routes

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const session = require('express-session');
 const {sessionConfig} = require('./config/session.js');
 
 const usuarioRoutes = require('./routes/usuarios-routes.js');
+const routes = require('./routes/routes.js');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,64 +14,11 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use('/css', express.static(path.join(__dirname, 'public', 'css')));
+app.use('/js', express.static(path.join(__dirname, 'public', 'js')));
 app.use('/img', express.static(path.join(__dirname, 'public', 'img')));
 app.use(session(sessionConfig));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/login.html'));
-});
-
-app.get('/buscar-empresas', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'buscar-empresas.html'));
-});
-
-app.get('/buscar-vagas', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'buscar-vagas.html'));
-});
-
-app.get('/cadastro', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'cadastro.html'));
-});
-
-app.get('/candidaturas', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'candidaturas.html'));
-});
-
-app.get('/empresa-home', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'empresa-home.html'));
-});
-
-app.get('/index', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'login.html'));
-});
-
-app.get('/minhas-vagas', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'minhas-vagas.html'));
-});
-
-app.get('/perfil-candidato', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'perfil-candidato.html'));
-});
-
-app.get('/perfil-empresa', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'perfil-empresa.html'));
-});
-
-app.get('/sobre-nos', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'sobre-nos.html'));
-});
-
-app.get('/ver-candidato', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'ver-candidato.html'));
-});
-
-app.get('/ver-empresa', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'ver-empresa.html'));
-});
+app.use('/', routes);
 
 app.use('/usuarios', usuarioRoutes);
 
